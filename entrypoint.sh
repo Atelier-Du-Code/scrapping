9688082ac8e5b3e317ec2a329527282ef655e39d
manon.avaullee@epitech.eu <manon.avaullee@epitech.eu>
2025-07-21 12:40:06 +0200

    Ajout correction OCR 8

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ echo "Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "Starting Gunicorn server..."
-exec gunicorn webapp.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn webapp.wsgi:application --bind 0.0.0.0:8000 --workers=3 --timeout=120
